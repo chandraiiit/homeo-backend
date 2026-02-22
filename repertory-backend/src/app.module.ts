104618f0da8e9075eda680e97a10dc2b0b3206fe
chandraiiit <ckccchaturvedi@gmail.com>
2026-02-22 17:44:02 +0530

    added typeorm and modules

--- repertory-backend/src/app.module.ts
+++ repertory-backend/src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { CasesModule } from './cases/cases.module';
+import { RubricsModule } from './rubrics/rubrics.module';
+import { RemediesModule } from './remedies/remedies.module';
+import { RepertorizationModule } from './repertorization/repertorization.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'yourpassword',
+      database: 'repertory_db',
+      autoLoadEntities: true,
+      synchronize: true, // turn off in production
+    }),
+    UsersModule,
+    AuthModule,
+    CasesModule,
+    RubricsModule,
+    RemediesModule,
+    RepertorizationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
